dff701de4968be3301ba3362f2996d859f510763
Ramanand Anand Mohare <ramanandmohare14@gmail.com>
2021-04-26 15:27:50 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,12 +3,10 @@ import pandas as pd
 
 def predictRuns(testInput):
     prediction = 0
-    ### Your Code Here ###
-    
-    #IPL team names list:
-    #team = ['Chennai Super Kings', 'Delhi Capitals', 'Kolkata Knight Riders','Mumbai Indians', 'Punjab Kings', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad']
 
+    ### Your Code Here ###
     data = pd.read_csv(r'C:\Users\Ramanand\OneDrive\Desktop\Python\inputFile.csv', squeeze = True)
+    
     #calculation of total runs of bat
     r1 = data['runs_off_bat'].sum()
     #print('r1: '+str(r1))
@@ -19,23 +17,21 @@ def predictRuns(testInput):
 
     #check for the batting team and set score1
     if(name == 'Chennai Super Kings'):
-            score1 = 0
+            score1 = 112.5
     elif(name == 'Delhi Capitals'):
-            score1 = 0
+            score1 = 110
     elif(name == 'Kolkata Knight Riders'):
-            score1 = 0
+            score1 = 101.33
     elif(name == 'Mumbai Indians'):
-            score1 = 0
+            score1 = 113.5
     elif(name == 'Punjab Kings'):
-            score1 = 0
+            score1 = 120
     elif(name == 'Rajasthan Royals'):
-            score1 = 0
+            score1 = 100
     elif(name == 'Royal Challengers Bangalore'):
-            score1 = 0
+            score1 = 95
     elif(name == 'Sunrisers Hyderabad'):
-            score1 = 0
-
-    #check for venue
+            score1 = 108.66
 
     #sum of other type of runs
     r2 = data['extras'].sum() + data['wides'].sum() + data['noballs'].sum() + data['byes'].sum() + data['legbyes'].sum()
@@ -49,7 +45,26 @@ def predictRuns(testInput):
     score2 = avg_r * 28
     #print(score2)
 
-    #wicket factor
+    #total number of wickets
+    count = 0
+    w = data['wicket_type']
+    for n in w:
+        if(isinstance(n, str) == True):
+                count += 1
+    #print(count)
+
+    #check how wicket affect the scoreboard
+    if(count < 4):
+        dn = count * 3
+        score2 = score2 - dn
+        #print(dn)
+        #print(score2)
+
+    if(count >= 4):
+        dn = count * 3
+        score2 = score2 - dn
+        #print(dn)
+        #print(score2)
 
     prediction = (score1 + score2)/2
     return prediction
